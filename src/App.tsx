033a69814ffc31abe86fefb7b90ec909b2064d6f
Constantino <larissaconstantino@yahoo.com.br>
2022-10-16 14:35:58 -0300

    design system

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+  <h1 className="font-bold 2xl text-violet-500">Ignite Lab - Design System</h1>
+
+  <button className="bg-cyan-500 md px-4 py-2 rounded text-white hover:bg-cyan-300">
+    Enviar
+  </button>
+  </>
+  )
+}
